e279b794a4a206f4f4190c922b4212402ed029c2
NotTrashraD <reski16@outlook.fr>
2023-12-17 04:01:59 +0100

    initial commit

---
+++ lua/arc9/common/attachments/ins2_vfg.lua
@@ -0,0 +1,24 @@
+ATT.PrintName = "Vertical Foregrip"
+ATT.CompactName = "VFG"
+ATT.Description = [[Makes gun less go bounce.]]
+
+ATT.Icon = Material("models/reshed/atts/icons/a_foregrip_ins_512.png", "mips smooth")
+
+ATT.Model = "models/weapons/reshed/grips/a_foregrip_ins.mdl"
+
+ATT.SortOrder = 1
+ATT.Category = {"ins2_rail"}
+
+ATT.RecoilPatternDriftMult = 0.75
+ATT.RecoilAutoControlMult = 1.15
+
+ATT.LHIK_Priority = 10
+ATT.LHIK = true
+
+ATT.ModelOffset = Vector(-30, -5.1, 2.5)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.AimDownSightsTimeMult = 1.05
+ATT.SprintToFireTimeMult = 1.075
+ATT.SpeedMult = 0.97
+ATT.RecoilMult = 0.9
---
+++ lua/arc9/common/attachments_bulk/ins2_optics.lua
@@ -0,0 +1,84 @@
+local ATT = {}
+
+----------------------------------------------------------------------------------
+
+--- REFLEX ---
+
+----------------------------------------------------------------------------------
+
+ATT = {}
+
+ATT.PrintName = "AIMPOINT RDS"
+ATT.CompactName = "AIMPOINT"
+ATT.Icon = Material("models/reshed/atts/icons/a_optic_aimpoint_512.png")
+ATT.Description = [[A snappy sight.]]
+ATT.SortOrder = 0
+
+ATT.Model = "models/weapons/reshed/optics/a_optic_aimpoint.mdl"
+
+ATT.Category = {"ins2_opticrail"}
+ATT.Folder = "REFLEX"
+
+-- Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(0, 9, -1.4),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ViewModelFOV = 60,
+    },
+}
+
+ATT.Scale = 1.2
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("models/reshed/atts/reticles/aimpoint_dot.png", "mips smooth")
+ATT.HoloSightSize = 200
+ATT.HoloSightColorable = false
+
+ATT.ModelOffset = Vector(0, 0, -0.1)
+
+ATT.SprintToFireTimeAdd = 0.035
+
+ARC9.LoadAttachment(ATT, "ins2_optic_aimpointrds")
+
+
+----------------------------------------------------------------------------------
+
+
+ATT = {}
+
+ATT.PrintName = "EOTECH 552"
+ATT.CompactName = "EO552"
+ATT.Icon = Material("models/reshed/atts/icons/a_optic_eotech_512.png")
+ATT.Description = [[A snappy sight.]]
+ATT.SortOrder = 0
+
+ATT.Model = "models/weapons/reshed/optics/a_optic_eotech.mdl"
+
+ATT.Category = {"ins2_opticrail"}
+ATT.Folder = "REFLEX"
+
+-- Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(0, 9, -1.4),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ViewModelFOV = 60,
+    },
+}
+
+ATT.Scale = 1.2
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("models/reshed/atts/reticles/eotech_reticule.png", "mips smooth")
+ATT.HoloSightSize = 500
+ATT.HoloSightColorable = false
+
+ATT.ModelOffset = Vector(0, 0, -0.1)
+
+ATT.SprintToFireTimeAdd = 0.035
+
+ARC9.LoadAttachment(ATT, "ins2_optic_eotech")
+
+
+
