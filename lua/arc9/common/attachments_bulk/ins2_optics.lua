caa8390416d0f0634e52cb8c669e1374d4763ab4
NotTrashraD <reski16@outlook.fr>
2023-12-18 06:32:14 +0100

    aaaaa

--- lua/arc9/common/attachments_bulk/ins2_optics.lua
+++ lua/arc9/common/attachments_bulk/ins2_optics.lua
@@ -82,3 +82,44 @@ ARC9.LoadAttachment(ATT, "ins2_optic_eotech")
 
 
 
+
+
+
+
+ATT = {}
+
+ATT.PrintName = "AIMPOINT COMPM4"
+ATT.CompactName = "COMPM4"
+ATT.Icon = Material("")
+ATT.Description = [[A snappy sight.]]
+ATT.SortOrder = 0
+
+ATT.Model = "models/weapons/reshed/optics/insscompm4.mdl"
+
+ATT.Category = {"ins2_opticrail"}
+ATT.Folder = "REFLEX"
+
+-- Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(0, 9, -1.7),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ViewModelFOV = 60,
+    },
+}
+
+ATT.Scale = 1.5
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("models/reshed/atts/reticles/aimpoint_dot.png", "mips smooth")
+ATT.HoloSightSize = 200
+ATT.HoloSightColorable = false
+
+ATT.ModelOffset = Vector(0, 0, -0.2)
+
+ATT.SprintToFireTimeAdd = 0.035
+
+ARC9.LoadAttachment(ATT, "ins2_optic_aimpointcompm4")
+
+
+
